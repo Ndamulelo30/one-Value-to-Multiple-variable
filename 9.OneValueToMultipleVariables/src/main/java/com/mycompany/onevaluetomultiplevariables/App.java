e77b25a2b262b2c7afde5c09812b98ad27df0034
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-16 14:27:31 +0200

    Add files via upload

---
+++ 9.OneValueToMultipleVariables/src/main/java/com/mycompany/onevaluetomultiplevariables/App.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.onevaluetomultiplevariables;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        int x, y, z;
+        x = y = z = 888;
+        System.out.println(x + y + z);
+    }
+}
